cb0cd434be7802021323e4975aca7ebdb2082a66
fikasp <fikasp@gmail.com>
2023-02-05 11:01:51 +0100

    edited shop

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { formatNumber as format } from './Tools';
 
-export default function Product ({ product, removeProduct }) {
+export default function Product ({ product, editProduct, removeProduct }) {
   return (
   <div className="product">
     <div className="product-name">{product.name}</div>
-    <div className="product-price">{format(product.price)}</div>
-    <button onClick={() => removeProduct(product.id)}>Remove</button>
+    <div className="product-price">{format(product.price)} $</div>
+    <div className="product-buttons">
+      <button onClick={() => editProduct(product.id)}>Edit</button>
+      <button onClick={() => removeProduct(product.id)}>Remove</button>
+    </div>
   </div>
   )
 };
--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -3,16 +3,16 @@ import { formatNumber as format } from './Tools'
 import Product from './Product';
 import Modal from './Modal';
 
-let index = 4;
+let index = 0;
 export default function ProductsList() {
 
   const [products, setProducts] = useState([
-    { id: 1, name: "Product 1", price: 10 },
-    { id: 2, name: "Product 2", price: 20 },
-    { id: 3, name: "Product 3", price: 30 },
+    { id: -3, name: "Product 1", price: 10 },
+    { id: -2, name: "Product 2", price: 20 },
+    { id: -1, name: "Product 3", price: 30 },
   ]);
-  const [newProductName, setNewProductName] = useState('');
-  const [newProductPrice, setNewProductPrice] = useState('');
+  const [newProduct, setNewProduct] = useState({name: '',price: ''});
+  const [editingProduct, setEditingProduct] = useState({});
   const [showModal, setShowModal] = useState(false);
   const [total, setTotal] = useState(0);
   const modalRef = React.useRef(null);
@@ -36,57 +36,107 @@ export default function ProductsList() {
 
   const addProduct = () => {
     index++;
-    if (!newProductName || !newProductPrice) {
+    if (!newProduct.name || !newProduct.price) {
       setShowModal(true);
       return;
     }
     setProducts([...products, { 
       id: index, 
-      name: newProductName, 
-      price: Number(newProductPrice)}
+      name: newProduct.name, 
+      price: newProduct.price}
     ]);
-    setNewProductName('');
-    setNewProductPrice('');
+    setNewProduct({name: '',price: ''});
     setShowModal(false)
+  };
+
+  const editProduct = (productId) => {
+    const product = products.find(p => p.id === productId);
+    setEditingProduct(product);
+  };
+
+  const updateProduct = () => {
+    setProducts(products.map((product) =>
+    product.id === editingProduct.id
+      ? editingProduct
+      : product
+      )
+    );
+    setEditingProduct({});
+  };
+
+  const cancelEditing = () => {
+    setEditingProduct({});
   };
 
   const removeProduct = productId => {
     setProducts(products.filter(product => product.id !== productId));
   };
 
-  const handleAddProduct = (event) => {
+  const handleEnter = callback => (event) => {
     if (event.key === "Enter") {
-      addProduct();
+      callback();
     }
   };
 
   return (
     <div className="product-list">
-      <div className="add-product">
+      { !editingProduct.id ? (
 
-        <input
-          type="text"
-          placeholder="Product Name"
-          value={newProductName}
-          onChange={e => setNewProductName(e.target.value)}
-          onKeyDown={handleAddProduct}
-        />
+        <div className="add-product">
+          <input
+            type="text"
+            placeholder="Product Name"
+            value={newProduct.name}
+            onChange={(e) => setNewProduct({...newProduct, name: e.target.value})}
+            onKeyDown={handleEnter(addProduct)}
+          />
+          <input
+            type="number"
+            placeholder="Product Price"
+            value={newProduct.price}
+            onChange={(e) => setNewProduct({...newProduct, price: Number(e.target.value)})}
+            onKeyDown={handleEnter(addProduct)}
+          />
+          <div className="buttons">
+            <button onClick={addProduct}>Add Product</button>
+          </div>
+        </div>
 
-        <input
-          type="number"
-          placeholder="Product Price"
-          value={newProductPrice}
-          onChange={e => setNewProductPrice(e.target.value)}
-          onKeyDown={handleAddProduct}
-        />
+        ) : (
+          
+        <div className="edit-product">
+          <input
+            type="text"
+            placeholder="Product Name"
+            value={editingProduct.name}
+            onChange={(e) => 
+              setEditingProduct({...editingProduct, name: e.target.value})}
+            onKeyDown={handleEnter(updateProduct)}            
+          />
+          <input
+            type="number"
+            placeholder="Product Price"
+            value={editingProduct.price}
+            onChange={(e) => 
+              setEditingProduct({ ...editingProduct, price: Number(e.target.value) })}
+            onKeyDown={handleEnter(updateProduct)}
+          />
+          <div className="buttons">
+            <button onClick={updateProduct}>Update</button>
+            <button onClick={cancelEditing}>Cancel</button>
+          </div>
+        </div>
 
-        <button onClick={addProduct}>Add Product</button>
-
-      </div>
+      )}
 
       <div>
         {products.map(product => (
-          <Product key={product.id} product={product} removeProduct={removeProduct} />
+          <Product 
+            key={product.id} 
+            product={product} 
+            removeProduct={removeProduct} 
+            editProduct={editProduct} 
+          />
         ))}
       </div>
 
